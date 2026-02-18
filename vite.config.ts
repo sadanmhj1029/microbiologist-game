e00e4dcd344d38b41608c0ba05bf2a94ecee6366
Sadan <sadanmhj1@gmail.com>
2026-02-18 22:16:11 +0530

    change

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/microbiologist-game/"
+  base: "/microbiologist-game/",
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
+  }
 })
